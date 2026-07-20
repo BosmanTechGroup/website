5625714b804c2b93818f9a34b6f684ab24db7ee6
Peter Bosman <info@peterbosman.be>
2026-07-20 06:22:50 +0200

    chore: added layout files to pages, main page for adding Header and Footer, login and register to add Metadata titles

---
+++ src/app/(front-page)/layout.tsx
@@ -0,0 +1,16 @@
+import Footer from "@/src/components/footer";
+import Header from "@/src/components/header";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
+}
---
+++ src/app/(login-pages)/login/layout.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Login - Bosman Tech Group",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <>{children}</>;
+}
