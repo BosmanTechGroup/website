ec32567ba65e46c2d2b80588ee541b7a6fc9c886
Peter Bosman <info@peterbosman.be>
2026-07-20 06:24:13 +0200

    chore: created about page

---
+++ src/app/(front-page)/over-ons/page.tsx
@@ -0,0 +1,123 @@
+import { companyInfo, socialIcons } from "@/src/lib/data";
+
+export default function AboutUsPage() {
+  return (
+    <section className="mx-auto max-w-7xl px-6 py-8">
+      <div className="text-center">
+        <div className="mb-16">
+          <h2 className="text-4xl pb-2 font-bold">Over ons</h2>
+          <p>{companyInfo.description}</p>
+        </div>
+        <div>
+          <h2 className="text-4xl pb-2 font-bold">Ons Team</h2>
+          {companyInfo.employees.map((employee) => (
+            <div key={employee.name} className="text-center mb-8">
+              <div className="rounded-3xl bg-white p-8 shadow-lg">
+                <div className="flex flex-col items-center gap-4 md:flex-row md:items-start">
+                  <img
+                    className="rounded-full"
+                    src={
+                      employee.image ||
+                      "/images/about-us/" +
+                        employee.firstName.toLowerCase() +
+                        ".webp"
+                    }
+                    alt={employee.firstName}
+                    width={200}
+                    height={200}
+                  />
+                  <div className="flex flex-col items-center pl-5 gap-2 md:items-start">
+                    <h3 className="text-2xl font-bold">{employee.name}</h3>
+                    <p className="text-lg text-slate-600">
+                      {employee.positions.join(" / ")}
+                    </p>
+                    <p className="text-left">{employee.bio}</p>
+                    {employee.socials && (
+                      <div className="mt-2 flex justify-center gap-4">
+                        {employee.socials.linkedin && (
+                          <a
+                            href={employee.socials.linkedin}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            <img
+                              width={32}
+                              height={32}
+                              src={socialIcons.linkedin}
+                              alt="LinkedIn"
+                            />
+                          </a>
+                        )}
+                        {employee.socials.github && (
+                          <a
+                            href={employee.socials.github}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            <img
+                              width={32}
+                              height={32}
+                              src={socialIcons.github}
+                              alt="GitHub"
+                            />
+                          </a>
+                        )}
+                        {employee.email && (
+                          <a
+                            href={`mailto:${employee.email}`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            <img
+                              width={32}
+                              height={32}
+                              src={socialIcons.mail}
+                              alt="Mail"
+                            />
+                          </a>
+                        )}
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="mt-16 grid gap-8 md:grid-cols-2 xl:grid-cols-3">
+        <div className="group rounded-3xl bg-white p-8 shadow-lg transition hover:-translate-y-2 hover:shadow-2xl">
+          <h3 className="text-xl font-bold">test title</h3>
+
+          <p className="mt-3 text-slate-600">test</p>
+
+          <button className="mt-8 font-semibold text-blue-600">
+            Meer informatie →
+          </button>
+        </div>
+
+        <div className="group rounded-3xl bg-white p-8 shadow-lg transition hover:-translate-y-2 hover:shadow-2xl">
+          <h3 className="text-xl font-bold">test title</h3>
+
+          <p className="mt-3 text-slate-600">test</p>
+
+          <button className="mt-8 font-semibold text-blue-600">
+            Meer informatie →
+          </button>
+        </div>
+
+        <div className="group rounded-3xl bg-white p-8 shadow-lg transition hover:-translate-y-2 hover:shadow-2xl">
+          <h3 className="text-xl font-bold">test title</h3>
+
+          <p className="mt-3 text-slate-600">test</p>
+
+          <button className="mt-8 font-semibold text-blue-600">
+            Meer informatie →
+          </button>
+        </div>
+
+      </div>
+    </section>
+  );
+}
