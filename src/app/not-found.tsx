944c03c5f55f9f519ca1b21506ce0a1a36634ff4
Peter Bosman <info@peterbosman.be>
2026-07-22 19:37:48 +0200

    chore: added translations and css to not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,9 @@
+import { useTranslations } from "next-intl";
 import Link from "next/link";
+import "@/src/app/globals.css";
 
 export default function NotFound() {
+  const tNotFound = useTranslations("NotFound");
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-4 min-h-screen items-center justify-center">
       <div className="hidden md:block md:col-span-2 h-full">
@@ -16,13 +19,13 @@ export default function NotFound() {
       <div className="w-full max-w-md h-100% rounded-2xl mx-auto">
         <h1 className="text-5xl font-extrabold md:text-7xl">404</h1>
         <p className="text-lg text-slate-600 mb-6">
-          Oeps! De pagina die je zoekt bestaat niet.
+          {tNotFound("message")}
         </p>
         <Link
           href="/"
           className="rounded-xl bg-blue-600 px-5 py-3 font-semibold text-white transition hover:bg-blue-700"
         >
-          Terug naar de homepage
+          {tNotFound("home-link")}
         </Link>
       </div>
     </div>
